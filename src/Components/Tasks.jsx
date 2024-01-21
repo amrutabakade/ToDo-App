eebe9b68ebc674ce70b9b2f64379869c3992cdc4
amrutabakade <131867195+amrutabakade@users.noreply.github.com>
2024-01-20 14:06:02 -1100

    first commit

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -8,12 +8,6 @@ return (<>
      <div className="col-sm-4">{prop2}</div>
      <div className="col-sm-2" ><button type="button" className="btn btn-danger" onClick={() =>{prop3(prop1)}}>Delete</button></div>
    </div>
-   {/* <div className="row">
-     <div className="col-sm-4">cooking</div>
-     <div className="col-sm-4">24/11/2023</div>
-     <div className="col-sm-2"><button type="button" className="btn btn-success">Done</button></div>
-     <div className="col-sm-2"><button type="button" className="btn btn-danger">Delete</button></div>
-   </div> */}
    </div>
 </>);
 }
